8903c645f79356ff21ac0827576f68107e455b30
wenwenchencj <52459985+wenwenchencj@users.noreply.github.com>
2019-07-04 00:43:12 +0800

    Add files via upload

---
+++ sketch_jul03a.ino
@@ -0,0 +1,222 @@
+#include <Morse.h>
+
+Morse morse(13)
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() {
+  String str = "";
+  String morse_s = "";
+  int i,t ,tem=0;
+  int n = 0;
+  while(Serial.available()>0)
+  {
+      temp = 1;
+      str += char(serial.read());
+      n++
+    }
+
+   if (temp)
+   {
+	for i in range(len(str)):
+      		if  A<=str[i]<=Z:
+			str[i]();
+		if str[i]==" ":
+			c_space();
+		if str[i]=="
+":
+			w_space()
+    }
+      
+}
+
+def A:
+{
+    dot;
+    dash  
+}
+
+def B:
+{
+  dash;
+  dot;
+  dot;
+  dot;
+}
+
+def C:
+{
+  dash;
+  dot;
+  dash;
+  dot;
+}
+
+def D:
+{
+  dash;
+  dot;
+  dot;
+}
+
+def E:
+{
+  dot;
+}
+   
+def F:
+{
+  dot;
+  dot;
+  dash;
+  dot;
+}  
+def G:
+{
+  dash;
+  dash;
+  dot;
+}
+  
+def H:
+{
+  dash;
+  dash
+  dash;
+  dash;
+}
+    
+def I:
+{
+  dash;
+  dash;
+}   
+def J:
+{
+  dot;
+  dash;
+  dash;
+  dash;
+}  
+def K:
+{
+  dash;
+  dot;
+  dash;
+}
+   
+def L:
+{
+  dot;
+  dash;
+  dot;
+  dot;
+}
+    
+def M:
+{
+  dash;
+  dash;
+}
+    
+def N:
+{
+  dash;
+  dot;
+}
+    
+def O:
+{
+  dash;
+  dash;
+  dash;
+}
+    
+def P:
+{
+  dot;
+  dash;
+  dash;
+  dot;
+}
+   
+def Q:
+{
+  dash;
+  dash;
+  dot;
+  dash;
+}
+
+def R:
+{
+  dot;
+  dash;
+  dash;
+  dot;
+    
+}
+
+def S:
+{
+    dot;
+    dot;
+    dot;
+}
+
+def T:
+{
+   dash;   
+}
+
+def U:
+{
+  dot;
+  dot;
+  dash;    
+}
+
+def V:
+{
+   dot;
+   dot;
+   dot;
+   dash;
+}
+
+def W:
+{
+   dot;
+   dash;
+   dash;
+}
+
+def X:
+{
+  dash;
+  dot;
+  dot;
+  dash;   
+}
+
+def Y:
+{
+   dash;
+   dot;
+   dash;
+   dash;
+}
+
+def Z:
+{
+   dash;
+   dash;
+   dot;
+   dot;
+}    
+   
+
+ 
